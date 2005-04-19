4cf03c6a1892e846506aa48ccdafc50490676262
bmcgough <bmcgough@kuali.org>
2005-04-19 17:15:00 -0400

    adding xstream as a dependency for now
    git-svn-id: https://svn.kuali.org/repos/kfs/legacy/dependencies/trunk@33363 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

---
+++ xstream-1.1.1/src/test/com/thoughtworks/acceptance/someobjects/HandlerManager.java
@@ -0,0 +1,20 @@
+package com.thoughtworks.acceptance.someobjects;
+
+import java.util.List;
+
+/**
+ *
+ * 
+ * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
+ *
+ * @version $Id: HandlerManager.java,v 1.1 2005-04-19 21:15:00 bmcgough Exp $
+ */
+public class HandlerManager
+{
+    List handlers;
+
+    public List getHandlers()
+    {
+        return handlers;
+    }
+}
---
+++ xstream-1.1.1/src/test/com/thoughtworks/acceptance/someobjects/Protocol.java
@@ -0,0 +1,18 @@
+package com.thoughtworks.acceptance.someobjects;
+
+/**
+ *
+ * 
+ * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
+ *
+ * @version $Id: Protocol.java,v 1.1 2005-04-19 21:15:00 bmcgough Exp $
+ */
+public class Protocol
+{
+    private String id;
+
+    public String getId()
+    {
+        return id;
+    }
+}
