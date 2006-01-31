836cab153e65a51b7953f3cab150ab83a318c87d
rkirkend <rkirkend@kuali.org>
2006-01-31 14:01:24 -0500

    *** empty log message ***
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.1.2@243 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/sys/document/workflow/KualiPostProcessor.java
+++ work/src/org/kuali/kfs/sys/document/workflow/KualiPostProcessor.java
@@ -53,41 +53,11 @@ public class KualiPostProcessor implements PostProcessorRemote {
             }
         } catch (Exception e) {
             LOG.error("Caught Exception handing StatusChangeEvent", e);
-            throw new RemoteException(e.getMessage());
+            throw new RuntimeException(e.getMessage());
         }
 
         return true;
     }
-
-//    private void writeOutDocumentStatusChange(DocumentRouteStatusChangeVO statusChangeEvent) {
-//        // write a record to a table with the docHeaderId and the EdenConstants.ROUTE_HEADER_PROCESSED_CD
-//        // it will be picked up on the other side -- then dealt with -- and finally deleted
-//        
-//        Context ctx = null;
-//        Connection conn = null;
-//        try {
-//            conn = KualiSpringServiceLocator.getDataSource().getConnection();
-//            PreparedStatement insertDocumentChangeEvent = conn.prepareStatement("INSERT INTO FP_DOC_STATUS_CHANGE_QUEUE_T VALUES (?,?,?,?,?)");
-//            insertDocumentChangeEvent.setLong(1, statusChangeEvent.getRouteHeaderId().longValue());
-//            insertDocumentChangeEvent.setLong(2, 1);
-//            insertDocumentChangeEvent.setString(3, statusChangeEvent.getNewRouteStatus());
-//            insertDocumentChangeEvent.setString(4, "N");
-//            insertDocumentChangeEvent.setString(5, "N");
-//            insertDocumentChangeEvent.executeUpdate();
-//        } catch (Exception e) {
-//            LOG.error("Error getting connection",e);
-//            throw new RuntimeException("An Error occurred during routing of this document",e);
-//        } finally {
-//            if (conn != null) {
-//                try {
-//                    conn.close();
-//                } catch (SQLException e1) {
-//                    // Ignore
-//                }
-//                conn = null;
-//            }
-//        }
-//    }
     
     public boolean doActionTaken(ActionTakenEventVO event) throws RemoteException {
         return true;
