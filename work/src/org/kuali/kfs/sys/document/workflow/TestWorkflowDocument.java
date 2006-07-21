848cce7614b38ecd868bfefa39d822b1b1bd4df1
unknown <unknown@kuali.org>
2006-07-21 19:02:37 -0400

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.4.6'.
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.4.6@2489 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/sys/document/workflow/TestWorkflowDocument.java
+++
@@ -1,9 +0,0 @@
-package org.kuali.workflow;
-
-import org.kuali.module.financial.document.InternalBillingDocument;
-
-public class TestWorkflowDocument extends InternalBillingDocument {
-
-    private static final long serialVersionUID = 5256426601708960189L;
-
-}
