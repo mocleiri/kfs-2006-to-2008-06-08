1c571c312ed369ac460dbc11b9ffd1542614e92f
tdurkin <tdurkin@kuali.org>
2006-09-06 18:38:57 -0400

    *** empty log message ***
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.11.2.3.2@3541 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/fp/document/validation/impl/PreEncumbranceDocumentRule.java
+++ work/src/org/kuali/kfs/fp/document/validation/impl/PreEncumbranceDocumentRule.java
@@ -22,24 +22,25 @@
  */
 package org.kuali.module.financial.rules;
 
-import org.apache.commons.lang.StringUtils;
-import org.kuali.Constants;
 import static org.kuali.Constants.BALANCE_TYPE_PRE_ENCUMBRANCE;
-import org.kuali.KeyConstants;
 import static org.kuali.PropertyConstants.REFERENCE_NUMBER;
 import static org.kuali.PropertyConstants.REVERSAL_DATE;
+import static org.kuali.core.util.AssertionUtils.assertThat;
+import static org.kuali.module.financial.rules.PreEncumbranceDocumentRuleConstants.PRE_ENCUMBRANCE_DOCUMENT_SECURITY_GROUPING;
+import static org.kuali.module.financial.rules.PreEncumbranceDocumentRuleConstants.RESTRICTED_OBJECT_TYPE_CODES;
+
+import org.apache.commons.lang.StringUtils;
+import org.kuali.Constants;
+import org.kuali.KeyConstants;
 import org.kuali.core.bo.AccountingLine;
 import org.kuali.core.bo.TargetAccountingLine;
 import org.kuali.core.datadictionary.BusinessObjectEntry;
 import org.kuali.core.document.Document;
 import org.kuali.core.document.TransactionalDocument;
 import org.kuali.core.rule.KualiParameterRule;
-import static org.kuali.core.util.AssertionUtils.assertThat;
 import org.kuali.core.util.GlobalVariables;
 import org.kuali.core.util.SpringServiceLocator;
 import org.kuali.module.financial.document.PreEncumbranceDocument;
-import static org.kuali.module.financial.rules.PreEncumbranceDocumentRuleConstants.PRE_ENCUMBRANCE_DOCUMENT_SECURITY_GROUPING;
-import static org.kuali.module.financial.rules.PreEncumbranceDocumentRuleConstants.RESTRICTED_OBJECT_TYPE_CODES;
 import org.kuali.module.gl.bo.GeneralLedgerPendingEntry;
 
 
@@ -79,7 +80,8 @@ public class PreEncumbranceDocumentRule extends TransactionalDocumentRuleBase {
 
     /**
      * This method checks that there is a value in a disencumbrance line's reference number field. This cannot be done by the
-     * DataDictionary validation because not all documents require it.  It does not validate the existence of the referenced document.
+     * DataDictionary validation because not all documents require it. It does not validate the existence of the referenced
+     * document.
      * 
      * @param accountingLine
      * 
@@ -214,8 +216,7 @@ public class PreEncumbranceDocumentRule extends TransactionalDocumentRuleBase {
     /**
      * limits only to expense object type codes
      * 
-     * @see IsDebitUtils#isDebitConsideringSection(TransactionalDocumentRuleBase, TransactionalDocument,
-     *      AccountingLine)
+     * @see IsDebitUtils#isDebitConsideringSection(TransactionalDocumentRuleBase, TransactionalDocument, AccountingLine)
      * 
      * @see org.kuali.core.rule.AccountingLineRule#isDebit(org.kuali.core.document.TransactionalDocument,
      *      org.kuali.core.bo.AccountingLine)
