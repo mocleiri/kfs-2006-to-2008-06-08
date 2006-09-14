6c4d56b8350c8fa1f752c60f2ee59931ccd33a6f
larevans <larevans@kuali.org>
2006-09-14 16:48:46 -0400

    KULGL-391
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.15.2@3696 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/gl/dataaccess/impl/OriginEntryGroupDaoOjb.java
+++ work/src/org/kuali/kfs/gl/dataaccess/impl/OriginEntryGroupDaoOjb.java
@@ -41,7 +41,7 @@ import org.springframework.orm.ojb.support.PersistenceBrokerDaoSupport;
 
 /**
  * @author Laran Evans <lc278@cornell.edu>
- * @version $Id: OriginEntryGroupDaoOjb.java,v 1.15 2006-09-06 06:50:17 abyrne Exp $ TODO Oracle Specific code here
+ * @version $Id: OriginEntryGroupDaoOjb.java,v 1.15.2.1 2006-09-14 20:48:46 larevans Exp $ TODO Oracle Specific code here
  */
 public class OriginEntryGroupDaoOjb extends PersistenceBrokerDaoSupport implements OriginEntryGroupDao {
     private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(OriginEntryGroupDaoOjb.class);
@@ -71,6 +71,17 @@ public class OriginEntryGroupDaoOjb extends PersistenceBrokerDaoSupport implemen
         sqlCommand(sql);
     }
 
+    public Collection<OriginEntryGroup> getGroupsFromSourceForDate(String sourceCode, Date date) {
+        LOG.debug("getGroupsFromSourceForDay() started");
+
+        Criteria criteria = new Criteria();
+        criteria.addEqualTo(DATE, date);
+        criteria.addEqualTo(SOURCE_CODE, sourceCode);
+        
+        return getPersistenceBrokerTemplate().getCollectionByQuery(QueryFactory.newQuery(OriginEntryGroup.class, criteria));
+        
+    }
+    
     /**
      * 
      * @see org.kuali.module.gl.dao.OriginEntryGroupDao#getOlderGroups(Date)
