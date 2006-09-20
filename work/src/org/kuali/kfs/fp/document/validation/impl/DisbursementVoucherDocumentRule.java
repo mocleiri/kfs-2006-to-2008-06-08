7c7bef9ed6259e3539121a41b1e835a01a691c97
abyrne <abyrne@kuali.org>
2006-09-20 16:09:33 -0400

    KULCFG-23
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.66.2.4.2@3787 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/fp/document/validation/impl/DisbursementVoucherDocumentRule.java
+++ work/src/org/kuali/kfs/fp/document/validation/impl/DisbursementVoucherDocumentRule.java
@@ -1336,7 +1336,7 @@ public class DisbursementVoucherDocumentRule extends TransactionalDocumentRuleBa
         KualiUser initUser = null;
         try {
 
-            initUser = SpringServiceLocator.getKualiUserService().getUser(new AuthenticationUserId(document.getDocumentHeader().getWorkflowDocument().getInitiatorNetworkId()));
+            initUser = SpringServiceLocator.getKualiUserService().getKualiUser(new AuthenticationUserId(document.getDocumentHeader().getWorkflowDocument().getInitiatorNetworkId()));
         }
         catch (UserNotFoundException e) {
             throw new RuntimeException("Document Initiator not found " + e.getMessage());
