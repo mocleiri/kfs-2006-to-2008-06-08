d05d460e0c8f1ef7f6b3cd3d1e2c9d622a978b7b
bnelson <bnelson@kuali.org>
2006-09-20 20:32:12 -0400

    KULCFG-63 remove all contact information from comments in kuali_project files
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.8.8@3799 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- test/unit/src/org/kuali/kfs/coa/service/SubAccountServiceTest.java
+++ test/unit/src/org/kuali/kfs/coa/service/SubAccountServiceTest.java
@@ -32,7 +32,7 @@ import org.kuali.test.WithTestSpringContext;
 /**
  * This class tests the SubAccount service.
  * 
- * @author Kuali Nervous System Team (kualidev@oncourse.iu.edu)
+ * @author Kuali Nervous System Team ()
  */
 @WithTestSpringContext
 public class SubAccountServiceTest extends KualiTestBaseWithFixtures {
