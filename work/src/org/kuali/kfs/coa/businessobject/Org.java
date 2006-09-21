2d938a6cec13d1862576c4fa0787ac3ae1c9164b
jkneal <jkneal@kuali.org>
2006-09-21 13:10:36 -0400

    Added logic on UniversalUser getters to also check if the objects uuid is in sync with primitive, if not, refresh the UniversalUser object.
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.14.2.1.2@3808 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/coa/businessobject/Org.java
+++ work/src/org/kuali/kfs/coa/businessobject/Org.java
@@ -329,7 +329,7 @@ public class Org extends BusinessObjectBase {
     }
 
     public UniversalUser getOrganizationManagerUniversal() {
-        if (organizationManagerUniversal == null) {
+        if (organizationManagerUniversal == null || !organizationManagerUniversal.getPersonUniversalIdentifier().equals(organizationManagerUniversalId))  {
             try {
                 organizationManagerUniversal = SpringServiceLocator.getKualiUserService().getUniversalUser(organizationManagerUniversalId);
             } catch (UserNotFoundException unfe) {}
