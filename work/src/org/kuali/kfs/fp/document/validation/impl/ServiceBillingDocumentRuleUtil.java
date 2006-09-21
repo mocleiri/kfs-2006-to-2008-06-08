804c3241c47d8fdf1d420aa6a5fb33c12fcb39ee
abyrne <abyrne@kuali.org>
2006-09-21 00:18:48 -0400

    merged in change from phase-1 branch
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.4.2.1.2@3802 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/fp/document/validation/impl/ServiceBillingDocumentRuleUtil.java
+++ work/src/org/kuali/kfs/fp/document/validation/impl/ServiceBillingDocumentRuleUtil.java
@@ -39,7 +39,7 @@ import org.kuali.module.financial.bo.ServiceBillingControl;
 /**
  * This class contains static helper methods for ServiceBillingDocumentRule and ServiceBillingDocumentAuthorizer.
  * 
- * @author Kuali Financial Transactions Team (kualidev@oncourse.iu.edu)
+ * @author Kuali Financial Transactions Team ()
  */
 public class ServiceBillingDocumentRuleUtil {
 
