d7d258f4b690b2f0a62f758f765ed56252129d67
jkneal <jkneal@kuali.org>
2006-09-21 13:10:36 -0400

    Added logic on UniversalUser getters to also check if the objects uuid is in sync with primitive, if not, refresh the UniversalUser object.
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.2.4@3808 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/module/bc/businessobject/BudgetConstructionHeader.java
+++ work/src/org/kuali/kfs/module/bc/businessobject/BudgetConstructionHeader.java
@@ -322,7 +322,7 @@ public class BudgetConstructionHeader extends BusinessObjectBase {
     }
     
     public UniversalUser getBudgetLockUser() {
-        if (budgetLockUser == null) {
+        if (budgetLockUser == null || !budgetLockUser.getPersonUniversalIdentifier().equals(budgetLockUserIdentifier)) {
             try {
                 budgetLockUser = SpringServiceLocator.getKualiUserService().getUniversalUser(budgetLockUserIdentifier);
             } catch (UserNotFoundException unfe) {}
@@ -361,7 +361,7 @@ public class BudgetConstructionHeader extends BusinessObjectBase {
     }
 
     public UniversalUser getBudgetTransactionLockUser() {
-        if (budgetTransactionLockUser == null) {
+        if (budgetTransactionLockUser == null || !budgetTransactionLockUser.getPersonUniversalIdentifier().equals(budgetTransactionLockUserIdentifier)) {
             try {
                 budgetTransactionLockUser = SpringServiceLocator.getKualiUserService().getUniversalUser(budgetTransactionLockUserIdentifier);
             } catch (UserNotFoundException unfe) {}
