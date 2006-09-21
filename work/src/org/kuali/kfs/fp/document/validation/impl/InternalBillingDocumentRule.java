f75a35ca5f0ad3e49d0186a27786a4ee982239b9
bnelson <bnelson@kuali.org>
2006-09-20 20:32:12 -0400

    KULCFG-63 remove all contact information from comments in kuali_project files
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.36.2@3799 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/fp/document/validation/impl/InternalBillingDocumentRule.java
+++ work/src/org/kuali/kfs/fp/document/validation/impl/InternalBillingDocumentRule.java
@@ -51,7 +51,7 @@ import org.kuali.module.financial.document.InternalBillingDocument;
 /**
  * Business rule(s) applicable to InternalBilling document.
  * 
- * @author Kuali Financial Transactions Team (kualidev@oncourse.iu.edu)
+ * @author Kuali Financial Transactions Team ()
  */
 public class InternalBillingDocumentRule extends TransactionalDocumentRuleBase {
 
