95cf1f7f6f6f78e6b4c1e6c7401c8fdbe0a26762
maynalem <maynalem@kuali.org>
2006-09-26 16:20:15 -0400

    unit testing: removed xml based fixtures and framework
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.7.8@3892 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- test/integration/src/org/kuali/kfs/fp/document/TransferOfFundsDocumentTest.java
+++ test/integration/src/org/kuali/kfs/fp/document/TransferOfFundsDocumentTest.java
@@ -22,24 +22,22 @@
  */
 package org.kuali.module.financial.document;
 
+import static org.kuali.test.fixtures.AccountingLineFixture.LINE1;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import org.kuali.core.bo.SourceAccountingLine;
-import org.kuali.core.bo.TargetAccountingLine;
 import org.kuali.core.document.Document;
 import org.kuali.core.document.TransactionalDocumentTestBase;
 import org.kuali.core.workflow.service.KualiWorkflowDocument;
-import org.kuali.test.parameters.AccountingLineParameter;
-import org.kuali.test.parameters.DocumentParameter;
-import org.kuali.test.parameters.TransactionalDocumentParameter;
-import org.kuali.test.WithTestSpringContext;
+import org.kuali.test.DocumentTestUtils;
 import org.kuali.test.TestsWorkflowViaDatabase;
+import org.kuali.test.WithTestSpringContext;
+import org.kuali.test.fixtures.AccountingLineFixture;
 import org.kuali.workflow.WorkflowTestUtils;
 
 import edu.iu.uis.eden.EdenConstants;
 import edu.iu.uis.eden.clientapp.vo.NetworkIdVO;
-
 /**
  * This class is used to test TransferOfFundsDocument.
  * 
@@ -47,182 +45,36 @@ import edu.iu.uis.eden.clientapp.vo.NetworkIdVO;
  */
 @WithTestSpringContext
 public class TransferOfFundsDocumentTest extends TransactionalDocumentTestBase {
-    public static final String COLLECTION_NAME = "TransferOfFundsDocumentTest.collection1";
-    public static final String USER_NAME = "user1";
-    public static final String DOCUMENT_PARAMETER = "documentParameter5";
-
-    private static final String USER_INIT = "user_unprivileged";
-    private static final String USER_APPROVE1 = "user_accountSet1";
-    private static final String USER_APPROVE2 = "user_accountSet2";
 
     // The set of Route Nodes that the test document will progress through
 
-    private static final String ADHOC = "Adhoc Routing";
     private static final String ACCOUNT_REVIEW = "Account Review";
     private static final String ORG_REVIEW = "Org Review";
-    private static final String SUB_FUND = "Sub Fund";
 
 
-    private static final String[] FIXTURE_COLLECTION_NAMES = { COLLECTION_NAME };
-
-    // AccountingLineParameter fixture members
-    private AccountingLineParameter _sourceLine1;
-    private AccountingLineParameter _sourceLine2;
-    private AccountingLineParameter _sourceLine3;
-    private AccountingLineParameter _targetLine1;
-    private AccountingLineParameter _targetLine2;
-    private AccountingLineParameter _targetLine3;
 
     // /////////////////////////////////////////////////////////////////////////
     // Fixture Methods Start Here //
     // /////////////////////////////////////////////////////////////////////////
-    public String[] getFixtureCollectionNames() {
-        return FIXTURE_COLLECTION_NAMES;
-    }
-
-    /**
-     * Fixture method to obtain a <code>@{link SourceAccountingLine}</code> instance that is for a different account than the current user belongs to.
-     * 
-     * @return SourceAccountingLine
-     */
-    protected SourceAccountingLine getSourceAccountingLineDifferentAccount() {
-        return (SourceAccountingLine) getSourceLine1().createLine();
-    }
-
-    /**
-     * Fixture method to obtain a <code>@{link TargetAccountingLine}</code> instance that is for a different account than the current user belongs to.
-     * 
-     * @return TargetAccountingLine
-     */
-    protected TargetAccountingLine getTargetAccountingLineDifferentAccount() {
-        return (TargetAccountingLine) getTargetLine1().createLine();
-    }
-
-    /**
-     * Accessor method for sourceLine1 <code>{@link AccountingLineParameter}</code> fixture.
-     * 
-     * @return AccountingLineParameter fixture to get
-     */
-    public AccountingLineParameter getSourceLine1() {
-        return _sourceLine1;
-    }
-
-    /**
-     * Accessor method for sourceLine1 <code>{@link AccountingLineParameter}</code> fixture.
-     * 
-     * @param AccountingLineParameter fixture to set
-     */
-    public void setSourceLine1(AccountingLineParameter p) {
-        _sourceLine1 = p;
-    }
-
-    /**
-     * Accessor method for sourceLine2 <code>{@link AccountingLineParameter}</code> fixture.
-     * 
-     * @return AccountingLineParameter fixture to get
-     */
-    public AccountingLineParameter getSourceLine2() {
-        return _sourceLine2;
-    }
-
-    /**
-     * Accessor method for sourceLine2 <code>{@link AccountingLineParameter}</code> fixture.
-     * 
-     * @param AccountingLineParameter fixture to set
-     */
-    public void setSourceLine2(AccountingLineParameter p) {
-        _sourceLine2 = p;
-    }
-
-    /**
-     * Accessor method for sourceLine3 <code>{@link AccountingLineParameter}</code> fixture.
-     * 
-     * @return AccountingLineParameter fixture to get
-     */
-    public AccountingLineParameter getSourceLine3() {
-        return _sourceLine3;
-    }
-
-    /**
-     * Accessor method for sourceLine3 <code>{@link AccountingLineParameter}</code> fixture.
-     * 
-     * @param AccountingLineParameter fixture to set
-     */
-    public void setSourceLine3(AccountingLineParameter p) {
-        _sourceLine3 = p;
-    }
-
-    /**
-     * Accessor method for targetLine1 <code>{@link AccountingLineParameter}</code> fixture.
-     * 
-     * @return AccountingLineParameter fixture to get
-     */
-    public AccountingLineParameter getTargetLine1() {
-        return _targetLine1;
-    }
-
-    /**
-     * Accessor method for targetLine1 <code>{@link AccountingLineParameter}</code> fixture.
-     * 
-     * @param AccountingLineParameter fixture to set
-     */
-    public void setTargetLine1(AccountingLineParameter p) {
-        _targetLine1 = p;
-    }
-
-    /**
-     * Accessor method for targetLine2 <code>{@link AccountingLineParameter}</code> fixture.
-     * 
-     * @return AccountingLineParameter fixture to get
-     */
-    public AccountingLineParameter getTargetLine2() {
-        return _targetLine2;
-    }
-
-    /**
-     * Accessor method for targetLine2 <code>{@link AccountingLineParameter}</code> fixture.
-     * 
-     * @param AccountingLineParameter fixture to set
-     */
-    public void setTargetLine2(AccountingLineParameter p) {
-        _targetLine2 = p;
-    }
-
-    /**
-     * Accessor method for targetLine3 <code>{@link AccountingLineParameter}</code> fixture.
-     * 
-     * @return AccountingLineParameter fixture to get
-     */
-    public AccountingLineParameter getTargetLine3() {
-        return _targetLine3;
-    }
-
-    /**
-     * Accessor method for targetLine3 <code>{@link AccountingLineParameter}</code> fixture.
-     * 
-     * @param AccountingLineParameter fixture to set
-     */
-    public void setTargetLine3(AccountingLineParameter p) {
-        _targetLine3 = p;
-    }
 
     /**
      * 
      * @see org.kuali.core.document.DocumentTestBase#getDocumentParameterFixture()
      */
-    public DocumentParameter getDocumentParameterFixture() {
-        return (TransactionalDocumentParameter) getFixtureEntryFromCollection(COLLECTION_NAME, DOCUMENT_PARAMETER).createObject();
+    public Document getDocumentParameterFixture() throws Exception{
+        return DocumentTestUtils.createTransactionalDocument(getDocumentService(), TransferOfFundsDocument.class, 2007, "06");
     }
 
     /**
      * 
      * @see org.kuali.core.document.TransactionalDocumentTestBase#getTargetAccountingLineParametersFromFixtures()
      */
-    public List getTargetAccountingLineParametersFromFixtures() {
-        ArrayList list = new ArrayList();
-        list.add(getTargetLine1());
-        // list.add( getTargetLine2() );
-        // list.add( getTargetLine3() );
+    @Override
+    public List<AccountingLineFixture> getTargetAccountingLineParametersFromFixtures() {
+        List<AccountingLineFixture> list = new ArrayList<AccountingLineFixture>();
+        list.add(LINE1);
+        // list.add( LINE2 );
+        // list.add( LINE3 );
         return list;
     }
 
@@ -230,11 +82,12 @@ public class TransferOfFundsDocumentTest extends TransactionalDocumentTestBase {
      * 
      * @see org.kuali.core.document.TransactionalDocumentTestBase#getSourceAccountingLineParametersFromFixtures()
      */
-    public List getSourceAccountingLineParametersFromFixtures() {
-        ArrayList list = new ArrayList();
-        list.add(getSourceLine1());
-        // list.add( getSourceLine2() );
-        // list.add( getSourceLine3() );
+    @Override
+    public List<AccountingLineFixture> getSourceAccountingLineParametersFromFixtures() {
+        List<AccountingLineFixture> list = new ArrayList<AccountingLineFixture>();
+        list.add(LINE1);
+        // list.add( LINE2 );
+        // list.add( LINE3 );
         return list;
     }
 
@@ -244,16 +97,9 @@ public class TransferOfFundsDocumentTest extends TransactionalDocumentTestBase {
      * @param String name of user to use.
      */
     protected String getTestUserName() {
-        return getUserName();
+        return getUserName().toString();
     }
 
-    /**
-     * 
-     * @see org.kuali.core.document.TransactionalDocumentTestBase#getUserName()
-     */
-    public String getUserName() {
-        return (String) getFixtureEntryFromCollection(COLLECTION_NAME, USER_NAME).createObject();
-    }
 
     // /////////////////////////////////////////////////////////////////////////
     // Fixture Methods End Here //
