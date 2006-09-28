0c81929728f32cbff779812fc6e2c4fc539754a5
dbeutel <dbeutel@kuali.org>
2006-09-27 23:58:04 -0400

    unit tests - add session to the WithTestSpringContext annotation, remove sessions from tests that don't use them, and fix a few tests
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.2.8@3933 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- test/unit/src/org/kuali/kfs/coa/service/FundGroupServiceTest.java
+++ test/unit/src/org/kuali/kfs/coa/service/FundGroupServiceTest.java
@@ -25,7 +25,7 @@ package org.kuali.module.chart.service;
 import org.kuali.core.service.KualiCodeService;
 import org.kuali.core.util.SpringServiceLocator;
 import org.kuali.module.chart.bo.FundGroup;
-import org.kuali.test.KualiTestBaseWithSession;
+import org.kuali.test.KualiTestBase;
 import org.kuali.test.WithTestSpringContext;
 
 /**
@@ -35,7 +35,7 @@ import org.kuali.test.WithTestSpringContext;
  */
 
 @WithTestSpringContext
-public class FundGroupServiceTest extends KualiTestBaseWithSession {
+public class FundGroupServiceTest extends KualiTestBase {
 
     KualiCodeService kualiCodeService;
 
