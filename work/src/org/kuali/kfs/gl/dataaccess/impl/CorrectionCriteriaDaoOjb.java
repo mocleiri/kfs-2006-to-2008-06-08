d473b584ab786dbc119ea430b1a82113b35536ea
aapotts <aapotts@kuali.org>
2007-01-26 15:19:42 -0500

    Renamed abstractdao to PlatformAwareDaoBaseOjb. refactored all daoOjbs to extend from it
     KULCFG-175
    
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.4.4.2.2@5300 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/gl/dataaccess/impl/CorrectionCriteriaDaoOjb.java
+++ work/src/org/kuali/kfs/gl/dataaccess/impl/CorrectionCriteriaDaoOjb.java
@@ -22,11 +22,11 @@ import java.util.List;
 import org.apache.ojb.broker.query.Criteria;
 import org.apache.ojb.broker.query.QueryByCriteria;
 import org.apache.ojb.broker.query.QueryFactory;
+import org.kuali.core.dao.ojb.PlatformAwareDaoBaseOjb;
 import org.kuali.module.gl.bo.CorrectionCriteria;
 import org.kuali.module.gl.dao.CorrectionCriteriaDao;
-import org.springframework.orm.ojb.support.PersistenceBrokerDaoSupport;
 
-public class CorrectionCriteriaDaoOjb extends PersistenceBrokerDaoSupport implements CorrectionCriteriaDao {
+public class CorrectionCriteriaDaoOjb extends PlatformAwareDaoBaseOjb implements CorrectionCriteriaDao {
     private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(CorrectionCriteriaDaoOjb.class);
 
     /**
