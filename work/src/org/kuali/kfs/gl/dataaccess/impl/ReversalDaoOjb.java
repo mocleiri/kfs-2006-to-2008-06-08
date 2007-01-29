6fdf7a0d452e07255e21f85c6e27991d517f76ee
abyrne <abyrne@kuali.org>
2007-01-29 16:40:30 -0500

    POP-19
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.4.2@5340 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/gl/dataaccess/impl/ReversalDaoOjb.java
+++ work/src/org/kuali/kfs/gl/dataaccess/impl/ReversalDaoOjb.java
@@ -22,8 +22,6 @@
 package org.kuali.module.gl.dao.ojb;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Date;
 import java.util.Iterator;
 
@@ -32,19 +30,13 @@ import org.apache.ojb.broker.query.QueryByCriteria;
 import org.apache.ojb.broker.query.QueryFactory;
 import org.apache.ojb.broker.query.ReportQueryByCriteria;
 import org.kuali.PropertyConstants;
+import org.kuali.core.dao.ojb.PlatformAwareDaoBaseOjb;
 import org.kuali.module.gl.bo.Entry;
-import org.kuali.module.gl.bo.OriginEntry;
-import org.kuali.module.gl.bo.OriginEntryGroup;
 import org.kuali.module.gl.bo.Reversal;
 import org.kuali.module.gl.bo.Transaction;
 import org.kuali.module.gl.dao.ReversalDao;
-import org.springframework.orm.ojb.support.PersistenceBrokerDaoSupport;
 
-/**
- * 
- * 
- */
-public class ReversalDaoOjb extends PersistenceBrokerDaoSupport implements ReversalDao {
+public class ReversalDaoOjb extends PlatformAwareDaoBaseOjb implements ReversalDao {
     private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(ReversalDaoOjb.class);
 
     private final static String UNIVERISITY_FISCAL_YEAR = "universityFiscalYear";
