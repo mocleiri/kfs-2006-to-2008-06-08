76ad37a729499759ebf40c8f39e75e17c839f321
jkneal <jkneal@kuali.org>
2007-02-05 15:25:32 -0500

    Cleanup of authorization packages
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.7.4@5639 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/fp/document/TransferOfFundsDocument.java
+++ work/src/org/kuali/kfs/fp/document/TransferOfFundsDocument.java
@@ -16,6 +16,7 @@
 package org.kuali.module.financial.document;
 
 import org.kuali.Constants;
+import org.kuali.core.document.AmountTotaling;
 import org.kuali.core.document.Copyable;
 import org.kuali.core.document.Correctable;
 import org.kuali.kfs.document.AccountingDocumentBase;
@@ -28,7 +29,7 @@ import org.kuali.kfs.document.AccountingDocumentBase;
  * fund groups which are not required either by the terms of a loan or by other external agreements. These transfers are the most
  * commonly used throughout the university.
  */
-public class TransferOfFundsDocument extends AccountingDocumentBase implements Copyable, Correctable{
+public class TransferOfFundsDocument extends AccountingDocumentBase implements Copyable, Correctable, AmountTotaling{
     private static final long serialVersionUID = -3871133713027969492L;
 
     /**
