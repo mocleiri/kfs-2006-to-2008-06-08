d2786ed8c1f997d5b641beadfaf48628cae5353d
aapotts <aapotts@kuali.org>
2007-02-08 14:03:41 -0500

    POP-46
    Resolved - problem with oracle-specific SQL code. Refactored to remove SQL and use services
    
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.15.2@5685 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/gl/dataaccess/impl/OriginEntryGroupDaoOjb.java
+++ work/src/org/kuali/kfs/gl/dataaccess/impl/OriginEntryGroupDaoOjb.java
@@ -49,31 +49,17 @@ public class OriginEntryGroupDaoOjb extends PlatformAwareDaoBaseOjb implements O
         super();
     }
 
-    /**
-     * 
-     * @see org.kuali.module.gl.dao.OriginEntryGroupDao#copyGroup(org.kuali.module.gl.bo.OriginEntryGroup,
-     *      org.kuali.module.gl.bo.OriginEntryGroup)
-     */
-    public void copyGroup(OriginEntryGroup fromGroup, OriginEntryGroup toGroup) {
-        LOG.debug("copyGroup() started");
-
-        String sql = "insert into GL_ORIGIN_ENTRY_T (ORIGIN_ENTRY_ID, OBJ_ID, VER_NBR, ORIGIN_ENTRY_GRP_ID, ACCOUNT_NBR,FDOC_NBR, FDOC_REF_NBR, FDOC_REF_TYP_CD, " + "FDOC_REVERSAL_DT, FDOC_TYP_CD, FIN_BALANCE_TYP_CD, FIN_COA_CD,FIN_OBJ_TYP_CD, FIN_OBJECT_CD, FIN_SUB_OBJ_CD, FS_ORIGIN_CD, FS_REF_ORIGIN_CD, " + "ORG_DOC_NBR, ORG_REFERENCE_ID,PROJECT_CD, SUB_ACCT_NBR, TRANSACTION_DT, TRN_DEBIT_CRDT_CD, TRN_ENCUM_UPDT_CD, TRN_ENTR_SEQ_NBR, TRN_LDGR_ENTR_AMT," + "TRN_LDGR_ENTR_DESC, UNIV_FISCAL_PRD_CD, UNIV_FISCAL_YR, TRN_SCRBBR_OFST_GEN_IND, BDGT_YR) " + "select gl_origin_entry_t_seq.nextval,'" + new Guid().toString() + "', 1, " + toGroup.getId() + ", ACCOUNT_NBR, FDOC_NBR, FDOC_REF_NBR, FDOC_REF_TYP_CD, FDOC_REVERSAL_DT, " + "FDOC_TYP_CD, FIN_BALANCE_TYP_CD, FIN_COA_CD, FIN_OBJ_TYP_CD, FIN_OBJECT_CD, FIN_SUB_OBJ_CD, FS_ORIGIN_CD, FS_REF_ORIGIN_CD, ORG_DOC_NBR,ORG_REFERENCE_ID, "
-                + "PROJECT_CD, SUB_ACCT_NBR, TRANSACTION_DT, TRN_DEBIT_CRDT_CD, TRN_ENCUM_UPDT_CD, TRN_ENTR_SEQ_NBR, TRN_LDGR_ENTR_AMT,TRN_LDGR_ENTR_DESC, UNIV_FISCAL_PRD_CD, " + "UNIV_FISCAL_YR, TRN_SCRBBR_OFST_GEN_IND, BDGT_YR from GL_ORIGIN_ENTRY_T where ORIGIN_ENTRY_GRP_ID = " + fromGroup.getId();
-
-        sqlCommand(sql);
-    }
-
     public Collection<OriginEntryGroup> getGroupsFromSourceForDate(String sourceCode, Date date) {
         LOG.debug("getGroupsFromSourceForDay() started");
 
         Criteria criteria = new Criteria();
         criteria.addEqualTo(DATE, date);
         criteria.addEqualTo(SOURCE_CODE, sourceCode);
-        
+
         return getPersistenceBrokerTemplate().getCollectionByQuery(QueryFactory.newQuery(OriginEntryGroup.class, criteria));
-        
+
     }
-    
+
     /**
      * 
      * @see org.kuali.module.gl.dao.OriginEntryGroupDao#getOlderGroups(Date)
