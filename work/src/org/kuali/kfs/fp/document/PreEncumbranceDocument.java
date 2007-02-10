423bf8f7d0c98cf2a7bf214d38fe6be41d164cfd
j2eemgr <j2eemgr@kuali.org>
2007-02-10 06:37:39 -0500

    automated update of standard license header
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.7.2@5705 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/fp/document/PreEncumbranceDocument.java
+++ work/src/org/kuali/kfs/fp/document/PreEncumbranceDocument.java
@@ -1,8 +1,6 @@
 /*
  * Copyright 2005-2006 The Kuali Foundation.
  * 
- * $Source: /opt/cvs/kfs/work/src/org/kuali/kfs/fp/document/PreEncumbranceDocument.java,v $
- * 
  * Licensed under the Educational Community License, Version 1.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -32,7 +30,7 @@ import org.kuali.module.gl.util.SufficientFundsItem;
  * have already been made.
  * 
  * 
- * @version $Id: PreEncumbranceDocument.java,v 1.7.2.3 2006-10-14 02:29:22 jbmorris Exp $
+ * @version $Id: PreEncumbranceDocument.java,v 1.7.2.4 2007-02-10 11:37:26 j2eemgr Exp $
  */
 public class PreEncumbranceDocument extends TransactionalDocumentBase {
     private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(PreEncumbranceDocument.class);
