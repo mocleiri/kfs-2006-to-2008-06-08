323119f412cde3fa25a9d06bf1eda2727bccfd92
j2eemgr <j2eemgr@kuali.org>
2007-02-10 06:37:39 -0500

    automated update of standard license header
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.13.2@5705 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/gl/service/OriginEntryGroupService.java
+++ work/src/org/kuali/kfs/gl/service/OriginEntryGroupService.java
@@ -1,8 +1,6 @@
 /*
  * Copyright 2005-2006 The Kuali Foundation.
  * 
- * $Source: /opt/cvs/kfs/work/src/org/kuali/kfs/gl/service/OriginEntryGroupService.java,v $
- * 
  * Licensed under the Educational Community License, Version 1.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -25,7 +23,7 @@ import org.kuali.module.gl.bo.OriginEntryGroup;
 
 /**
  *  
- * @version $Id: OriginEntryGroupService.java,v 1.13.2.5 2006-10-14 02:29:36 jbmorris Exp $
+ * @version $Id: OriginEntryGroupService.java,v 1.13.2.6 2007-02-10 11:37:34 j2eemgr Exp $
  */
 
 public interface OriginEntryGroupService {
