36e238aa057c92e9cd456fcb4d1723ce45e5ffbe
rkirkend <rkirkend@kuali.org>
2007-03-07 16:21:47 -0500

    merged head into pre-extraction-branch
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.24.2@6074 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/module/purap/document/PurchasingDocumentBase.java
+++ work/src/org/kuali/kfs/module/purap/document/PurchasingDocumentBase.java
@@ -155,6 +155,7 @@ public abstract class PurchasingDocumentBase extends PurchasingAccountsPayableDo
         this.refreshReferenceObject("chartOfAccounts");
         this.refreshReferenceObject("organization");
         this.refreshReferenceObject("deliveryCampus");
+        this.refreshReferenceObject("vendorContract");
     }
 
     /**
@@ -1751,7 +1752,7 @@ public abstract class PurchasingDocumentBase extends PurchasingAccountsPayableDo
         }
        
         //if the user entered something set line number to that
-        if(itemLinePosition>1&&itemLinePosition<items.size()) {
+        if(itemLinePosition>0&&itemLinePosition<items.size()) {
             itemLinePosition = item.getItemLineNumber() - 1;
         }
         
