611ca72d5f9c2483cb960e7ef95a268fab136947
rkirkend <rkirkend@kuali.org>
2007-03-14 16:16:02 -0400

    Moved KFS SpringServiceLocator to KFS package.  Moved KNS methods to KNSSpringService Locator.  Separated noncore dependencies from core.
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.38.2@6192 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- test/unit/src/org/kuali/kfs/fp/document/service/CashManagementServiceTest.java
+++ test/unit/src/org/kuali/kfs/fp/document/service/CashManagementServiceTest.java
@@ -30,11 +30,11 @@ import org.kuali.core.exceptions.ValidationException;
 import org.kuali.core.util.GlobalVariables;
 import org.kuali.core.util.KualiDecimal;
 
-import static org.kuali.core.util.SpringServiceLocator.getBusinessObjectService;
-import static org.kuali.core.util.SpringServiceLocator.getCashDrawerService;
-import static org.kuali.core.util.SpringServiceLocator.getCashManagementService;
-import static org.kuali.core.util.SpringServiceLocator.getCashReceiptService;
-import static org.kuali.core.util.SpringServiceLocator.getDocumentService;
+import static org.kuali.kfs.util.SpringServiceLocator.getBusinessObjectService;
+import static org.kuali.kfs.util.SpringServiceLocator.getDocumentService;
+import static org.kuali.kfs.util.SpringServiceLocator.getCashDrawerService;
+import static org.kuali.kfs.util.SpringServiceLocator.getCashManagementService;
+import static org.kuali.kfs.util.SpringServiceLocator.getCashReceiptService;
 
 import org.kuali.module.financial.bo.BankAccount;
 import org.kuali.module.financial.bo.CashDrawer;
