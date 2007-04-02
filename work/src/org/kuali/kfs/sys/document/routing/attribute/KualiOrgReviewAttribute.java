172dc4337ffbf03a5bfd971efd2cbb7f96739821
ewestfal <ewestfal@kuali.org>
2007-04-02 17:23:13 -0400

    Added the missing MassRuleAttribute interface to the KualiOrgReviewAttribute.
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.45.2@6488 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/sys/document/routing/attribute/KualiOrgReviewAttribute.java
+++ work/src/org/kuali/kfs/sys/document/routing/attribute/KualiOrgReviewAttribute.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2006-2007 The Kuali Foundation.
- * 
+ *
  * Licensed under the Educational Community License, Version 1.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl1.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -47,6 +47,7 @@ import edu.iu.uis.eden.WorkflowServiceErrorImpl;
 import edu.iu.uis.eden.doctype.DocumentType;
 import edu.iu.uis.eden.lookupable.Field;
 import edu.iu.uis.eden.lookupable.Row;
+import edu.iu.uis.eden.plugin.attributes.MassRuleAttribute;
 import edu.iu.uis.eden.plugin.attributes.WorkflowAttribute;
 import edu.iu.uis.eden.routeheader.DocumentContent;
 import edu.iu.uis.eden.routetemplate.RouteContext;
@@ -60,7 +61,7 @@ import edu.iu.uis.eden.util.Utilities;
  *
  *
  */
-public class KualiOrgReviewAttribute implements WorkflowAttribute {
+public class KualiOrgReviewAttribute implements WorkflowAttribute, MassRuleAttribute {
 
     static final long serialVersionUID = 1000;
 
