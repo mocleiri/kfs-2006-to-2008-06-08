0bc34e7b639f3d464f42012cb681cf274e8f3bec
abyrne <abyrne@kuali.org>
2007-05-15 00:12:49 -0400

    head merge
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.31.2@7332 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/module/purap/document/PurchasingAccountsPayableDocumentBase.java
+++ work/src/org/kuali/kfs/module/purap/document/PurchasingAccountsPayableDocumentBase.java
@@ -15,6 +15,7 @@
  */
 package org.kuali.module.purap.document;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -26,9 +27,14 @@ import org.kuali.core.service.NoteService;
 import org.kuali.core.util.KualiDecimal;
 import org.kuali.core.util.ObjectUtils;
 import org.kuali.core.util.TypedArrayList;
+import org.kuali.kfs.bo.SourceAccountingLine;
 import org.kuali.kfs.document.AccountingDocumentBase;
 import org.kuali.kfs.util.SpringServiceLocator;
+import org.kuali.module.purap.bo.CreditMemoView;
+import org.kuali.module.purap.bo.PaymentRequestView;
+import org.kuali.module.purap.bo.PurchaseOrderView;
 import org.kuali.module.purap.bo.PurchasingApItem;
+import org.kuali.module.purap.bo.RequisitionView;
 import org.kuali.module.purap.bo.Status;
 import org.kuali.module.purap.bo.StatusHistory;
 import org.kuali.module.vendor.bo.VendorDetail;
@@ -45,17 +51,36 @@ public abstract class PurchasingAccountsPayableDocumentBase extends AccountingDo
     private Integer vendorHeaderGeneratedIdentifier;
     private Integer vendorDetailAssignedIdentifier;
     private String vendorCustomerNumber;
+    private String vendorName;
+    private String vendorLine1Address;
+    private String vendorLine2Address;
+    private String vendorCityName;
+    private String vendorStateCode;
+    private String vendorPostalCode;
+    private String vendorCountryCode;
+    private Integer accountsPayablePurchasingDocumentLinkIdentifier;
+
+    // NOT PERSISTED IN DB
+    private String vendorNumber; 
 
     // COMMON ELEMENTS
     protected List statusHistories;
 
     // COLLECTIONS
     private List<PurchasingApItem> items;
-    
+    private List<RequisitionView> relatedRequisitionViews;
+    private List<PurchaseOrderView> relatedPurchaseOrderViews;
+    private List<PaymentRequestView> relatedPaymentRequestViews;
+    private List<CreditMemoView> relatedCreditMemoViews;
+    private List<SourceAccountingLine> summaryAccounts;
+
     // REFERENCE OBJECTS
     private Status status;
     private VendorDetail vendorDetail;
 
+    // STATIC
+    public transient String [] belowTheLineTypes;
+    
     // CONSTRUCTORS
     public PurchasingAccountsPayableDocumentBase() {
         items = new TypedArrayList(getItemClass());
@@ -63,9 +88,14 @@ public abstract class PurchasingAccountsPayableDocumentBase extends AccountingDo
     }
     
     public KualiDecimal getTotalDollarAmount() {
-        //FIXME get real total
-//        return Constants.ZERO;
-        return new KualiDecimal(100);
+        //TODO: the easy total dollar amount for now (i.e. not taking into account inactive etc
+        //do more analysis and make better
+        KualiDecimal total = new KualiDecimal(BigDecimal.ZERO);
+        for (PurchasingApItem item : items) {
+            KualiDecimal itemTotal = (item.getExtendedPrice()==null)?KualiDecimal.ZERO:item.getExtendedPrice();
+            total = total.add(itemTotal);
+        }
+        return total;
     }
 
     /**
@@ -102,6 +132,17 @@ public abstract class PurchasingAccountsPayableDocumentBase extends AccountingDo
                 throw new RuntimeException("Unable to create or save status history note " + e.getMessage());
             }
         }
+    }
+
+    public String getVendorNumber() {
+        if (ObjectUtils.isNotNull(vendorDetail)) {
+            return vendorDetail.getVendorNumber();
+        }
+        return "";
+    }
+
+    public void setVendorNumber(String vendorNumber) {
+        this.vendorNumber = vendorNumber;
     }
 
     // GETTERS AND SETTERS
@@ -221,6 +262,7 @@ public abstract class PurchasingAccountsPayableDocumentBase extends AccountingDo
         this.items = items;
     }
 
+    /*
     public void addItem(PurchasingApItem item) {
         int itemLinePosition = items.size();
         if(item.getItemLineNumber()!=null) {
@@ -238,6 +280,16 @@ public abstract class PurchasingAccountsPayableDocumentBase extends AccountingDo
         items.add(itemLinePosition,item);
         renumberItems(itemLinePosition);
     }
+    */
+    
+    public void addItem(PurchasingApItem item) {
+        int itemLinePosition = getItemLinePosition();
+        if(ObjectUtils.isNotNull(item.getItemLineNumber())) {
+            itemLinePosition = item.getItemLineNumber().intValue()-1;
+        }
+        items.add(itemLinePosition,item);
+        renumberItems(itemLinePosition);
+    }
     
     public void deleteItem(int lineNum) {
         if(items.remove(lineNum)==null) {
@@ -249,8 +301,33 @@ public abstract class PurchasingAccountsPayableDocumentBase extends AccountingDo
     public void renumberItems(int start) {
         for (int i = start; i<items.size(); i++) {
             PurchasingApItem item = (PurchasingApItem)items.get(i);
-            item.setItemLineNumber(new Integer(i+1));
+            //only set the item line number for above the line items
+            item.refreshNonUpdateableReferences();
+            if (item.getItemType().isItemTypeAboveTheLineIndicator()) {
+                item.setItemLineNumber(new Integer(i+1));
+            }
         }
+    }
+    
+    /** 
+     * 
+     * This method helps to determine the item line position if the user
+     * did not specify the line number on an above the line items before
+     * clicking on the add button. It subtracts the number of the below
+     * the line items on the list with the total item list size.
+     * 
+     * @return int the item line position of the last(highest) line number of above
+     *         the line items.
+     */
+    private int getItemLinePosition() {
+        int belowTheLineCount = 0;
+        for (PurchasingApItem item: items) {
+            item.refreshNonUpdateableReferences();
+            if (!item.getItemType().isItemTypeAboveTheLineIndicator()) {
+                belowTheLineCount++;
+            }
+        }
+        return items.size() - belowTheLineCount;
     }
     
     public PurchasingApItem getItem(int pos) {
@@ -272,14 +349,7 @@ public abstract class PurchasingAccountsPayableDocumentBase extends AccountingDo
         }
         return (PurchasingApItem)items.get(pos);
     }
-    public KualiDecimal getTotal() {
-        KualiDecimal total = new KualiDecimal("0");
-        for (PurchasingApItem item : items) {
-           total = total.add(item.getExtendedPrice());
-       }
-       return total;
-    }
-
+    
     public abstract Class getItemClass();
 
     /**
@@ -297,6 +367,154 @@ public abstract class PurchasingAccountsPayableDocumentBase extends AccountingDo
         }
         return (accounts==null)?new ArrayList():accounts;
     }
+
+    public String getVendorCityName() {
+        return vendorCityName;
+    }
+
+    public void setVendorCityName(String vendorCityName) {
+        this.vendorCityName = vendorCityName;
+    }
+
+    public String getVendorCountryCode() {
+        return vendorCountryCode;
+    }
+
+    public void setVendorCountryCode(String vendorCountryCode) {
+        this.vendorCountryCode = vendorCountryCode;
+    }
+
+    public String getVendorLine1Address() {
+        return vendorLine1Address;
+    }
+
+    public void setVendorLine1Address(String vendorLine1Address) {
+        this.vendorLine1Address = vendorLine1Address;
+    }
+
+    public String getVendorLine2Address() {
+        return vendorLine2Address;
+    }
+
+    public void setVendorLine2Address(String vendorLine2Address) {
+        this.vendorLine2Address = vendorLine2Address;
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+
+    public String getVendorPostalCode() {
+        return vendorPostalCode;
+    }
+
+    public void setVendorPostalCode(String vendorPostalCode) {
+        this.vendorPostalCode = vendorPostalCode;
+    }
+
+    public String getVendorStateCode() {
+        return vendorStateCode;
+    }
+
+    public void setVendorStateCode(String vendorStateCode) {
+        this.vendorStateCode = vendorStateCode;
+    }
     
-    
+    /**
+     * Gets the accountsPayablePurchasingDocumentLinkIdentifier attribute. 
+     * @return Returns the accountsPayablePurchasingDocumentLinkIdentifier.
+     */
+    public Integer getAccountsPayablePurchasingDocumentLinkIdentifier() {
+        return accountsPayablePurchasingDocumentLinkIdentifier;
+    }
+
+    /**
+     * Sets the accountsPayablePurchasingDocumentLinkIdentifier attribute value.
+     * @param accountsPayablePurchasingDocumentLinkIdentifier The accountsPayablePurchasingDocumentLinkIdentifier to set.
+     */
+    public void setAccountsPayablePurchasingDocumentLinkIdentifier(Integer accountsPayablePurchasingDocumentLinkIdentifier) {
+        this.accountsPayablePurchasingDocumentLinkIdentifier = accountsPayablePurchasingDocumentLinkIdentifier;
+    }
+
+    public List<RequisitionView> getRelatedRequisitionViews() {
+        if (!(this instanceof RequisitionDocument) && relatedRequisitionViews == null) {
+            relatedRequisitionViews = new TypedArrayList(RequisitionView.class);
+            List<RequisitionView> tmpViews = SpringServiceLocator.getPurapService().getRelatedViews(RequisitionView.class, accountsPayablePurchasingDocumentLinkIdentifier);
+            for (RequisitionView view : tmpViews) {
+                relatedRequisitionViews.add(view);
+            }
+        }
+        return relatedRequisitionViews;
+    }
+
+    public List<CreditMemoView> getRelatedCreditMemoViews() {
+        if (!(this instanceof CreditMemoDocument) && relatedCreditMemoViews == null) {
+            relatedCreditMemoViews = new TypedArrayList(CreditMemoView.class);
+            List<CreditMemoView> tmpViews = SpringServiceLocator.getPurapService().getRelatedViews(CreditMemoView.class, accountsPayablePurchasingDocumentLinkIdentifier);
+            for (CreditMemoView view : tmpViews) {
+                relatedCreditMemoViews.add(view);
+            }
+        }
+        return relatedCreditMemoViews;
+    }
+
+    public List<PaymentRequestView> getRelatedPaymentRequestViews() {
+        if (!(this instanceof PaymentRequestDocument) && relatedPaymentRequestViews == null) {
+            relatedPaymentRequestViews = new TypedArrayList(PaymentRequestView.class);
+            List<PaymentRequestView> tmpViews = SpringServiceLocator.getPurapService().getRelatedViews(PaymentRequestView.class, accountsPayablePurchasingDocumentLinkIdentifier);
+            for (PaymentRequestView view : tmpViews) {
+                relatedPaymentRequestViews.add(view);
+            }
+        }
+        return relatedPaymentRequestViews;
+    }
+
+    public List<PurchaseOrderView> getRelatedPurchaseOrderViews() {
+        if (!(this instanceof PurchaseOrderDocument) && relatedPurchaseOrderViews == null) {
+            relatedPurchaseOrderViews = new TypedArrayList(PurchaseOrderView.class);
+            List<PurchaseOrderView> tmpViews = SpringServiceLocator.getPurapService().getRelatedViews(PurchaseOrderView.class, accountsPayablePurchasingDocumentLinkIdentifier);
+            for (PurchaseOrderView view : tmpViews) {
+                relatedPurchaseOrderViews.add(view);
+            }
+        }
+        return relatedPurchaseOrderViews;
+    }
+
+    /**
+     * Gets the belowTheLineTypes attribute. 
+     * @return Returns the belowTheLineTypes.
+     */
+    public String[] getBelowTheLineTypes() {
+        if(this.belowTheLineTypes==null) {
+            this.belowTheLineTypes = SpringServiceLocator.getPurapService().getBelowTheLineForDocument(this);
+        }
+        return belowTheLineTypes;
+    }
+
+    /**
+     * @see org.kuali.kfs.document.AccountingDocumentBase#getSourceAccountingLineClass()
+     */
+//    @Override
+//    public abstract Class getSourceAccountingLineClass();
+
+    /**
+     * Gets the summaryAccounts attribute this is used by the summary accounts method 
+     * @return Returns the summaryAccounts.
+     */
+    public List<SourceAccountingLine> getSummaryAccounts() {
+        return summaryAccounts;
+    }
+
+    /**
+     * Sets the summaryAccounts attribute value.
+     * @param summaryAccounts The summaryAccounts to set.
+     */
+    public void setSummaryAccounts(List<SourceAccountingLine> summaryAccounts) {
+        this.summaryAccounts = summaryAccounts;
+    }
+        
 }
