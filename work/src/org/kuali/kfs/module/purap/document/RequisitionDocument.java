f9cff8acd573cdcbcb80b2f9ac09dbfb688dc364
abyrne <abyrne@kuali.org>
2007-06-18 16:09:21 -0400

    head merge
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.89.2@7950 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/module/purap/document/RequisitionDocument.java
+++ work/src/org/kuali/kfs/module/purap/document/RequisitionDocument.java
@@ -619,5 +619,20 @@ public class RequisitionDocument extends PurchasingDocumentBase implements Copya
         return chartAcct;
     }
     
+    /**
+     * USED FOR ROUTING ONLY
+     * @deprecated
+     */
+    public String getStatusDescription() {
+        return "";
+}
+
+    /**
+     * USED FOR ROUTING ONLY
+     * @deprecated
+     */
+    public void setStatusDescription(String statusDescription) {
+    }
+
 }
 
