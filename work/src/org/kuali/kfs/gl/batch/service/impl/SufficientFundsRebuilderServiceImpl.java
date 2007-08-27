5d34d70de2a4d51c8928a9c1574736288088e177
jkeller <jkeller@kuali.org>
2007-08-27 17:00:15 -0400

    KULRNE-5009 : further system parameter refactoring
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.39.8@9301 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/gl/batch/service/impl/SufficientFundsRebuilderServiceImpl.java
+++ work/src/org/kuali/kfs/gl/batch/service/impl/SufficientFundsRebuilderServiceImpl.java
@@ -82,7 +82,7 @@ public class SufficientFundsRebuilderServiceImpl implements SufficientFundsRebui
     }
 
     private Integer getFiscalYear() {
-        String val = kualiConfigurationService.getApplicationParameterValue(KFSConstants.SUFFICIENT_FUND_REBUILDER_SCRIPT, GLConstants.ColumnNames.UNIVERSITY_FISCAL_YEAR);
+        String val = kualiConfigurationService.getParameterValue(KFSConstants.GL_NAMESPACE, GLConstants.ColumnNames.UNIVERSITY_FISCAL_YEAR);
         int yr = Integer.parseInt(val);
         return new Integer(yr);
     }
