c119b036f5dfd0c35d17abedc7d1494981bf8ce7
jbeltran <jbeltran@kuali.org>
2007-08-28 00:10:53 -0400

    *** empty log message ***
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.13.2@9307 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/module/ld/document/web/struts/LaborCorrectionAction.java
+++ work/src/org/kuali/kfs/module/ld/document/web/struts/LaborCorrectionAction.java
@@ -94,7 +94,7 @@ public class LaborCorrectionAction extends CorrectionAction{
             CorrectionAction.kualiConfigurationService = SpringContext.getBean(KualiConfigurationService.class);
         }
 
-        request.setAttribute("debug", Boolean.valueOf(kualiConfigurationService.getApplicationParameterIndicator("GL.GLCP", "GL.DEBUG")));
+        request.setAttribute("debug", Boolean.valueOf(kualiConfigurationService.getIndicatorParameter(KFSConstants.GL_NAMESPACE, "GL.DEBUG")));
 
         CorrectionForm rForm = (CorrectionForm) form;
         LOG.debug("execute() methodToCall: " + rForm.getMethodToCall());
