8c66022ae349148337625f921092c1dbe4b91cb2
jkeller <jkeller@kuali.org>
2007-09-05 13:50:02 -0400

    merged in changes between Root_kfs-sys-param-work-br and merge-kfs-sys-param-work-br-1 from HEAD
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.29.2@9461 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/module/ld/service/impl/LaborOriginEntryServiceImpl.java
+++ work/src/org/kuali/kfs/module/ld/service/impl/LaborOriginEntryServiceImpl.java
@@ -150,7 +150,7 @@ public class LaborOriginEntryServiceImpl implements LaborOriginEntryService {
 
     public Iterator<LaborOriginEntry> getEntriesByGroupAccountOrder(OriginEntryGroup oeg) {
         LOG.debug("getEntriesByGroupAccountOrder() started");
-        Iterator returnVal = originEntryDao.getEntriesByGroup(oeg, OriginEntryDao.SORT_ACCOUNT);
+        Iterator returnVal = laborOriginEntryDao.getEntriesByGroup(oeg, OriginEntryDao.SORT_ACCOUNT);
 
         return returnVal;
     }
@@ -305,7 +305,7 @@ public class LaborOriginEntryServiceImpl implements LaborOriginEntryService {
             return ledgerEntryHolder;
         }
 
-        Iterator entrySummaryIterator = originEntryDao.getSummaryByGroupId(groupIdList);
+        Iterator entrySummaryIterator = laborOriginEntryDao.getSummaryByGroupId(groupIdList);
         while (entrySummaryIterator.hasNext()) {
             Object[] entrySummary = (Object[]) entrySummaryIterator.next();
             LedgerEntry ledgerEntry = LedgerEntry.buildLedgerEntry(entrySummary);
