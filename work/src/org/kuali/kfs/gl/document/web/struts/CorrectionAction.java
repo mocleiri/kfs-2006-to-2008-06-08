8bc18dbe60694282cbc5f51c8035e5add25d940d
jkeller <jkeller@kuali.org>
2007-09-13 14:35:16 -0400

    KULRNE-5009 : finished removing the old parameter calls
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.91.2@9654 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/gl/document/web/struts/CorrectionAction.java
+++ work/src/org/kuali/kfs/gl/document/web/struts/CorrectionAction.java
@@ -103,7 +103,7 @@ public class CorrectionAction extends KualiDocumentActionBase implements KualiTa
             CorrectionAction.kualiConfigurationService = SpringContext.getBean(KualiConfigurationService.class);
         }
 
-        request.setAttribute("debug", Boolean.valueOf(kualiConfigurationService.getIndicatorParameter(KFSConstants.GL_NAMESPACE, "GL.DEBUG")));
+        request.setAttribute("debug", Boolean.valueOf(kualiConfigurationService.getIndicatorParameter(KFSConstants.GL_NAMESPACE, KFSConstants.Components.NOT_APPLICABLE, "GL.DEBUG")));
 
         CorrectionForm rForm = (CorrectionForm) form;
         LOG.debug("execute() methodToCall: " + rForm.getMethodToCall());
