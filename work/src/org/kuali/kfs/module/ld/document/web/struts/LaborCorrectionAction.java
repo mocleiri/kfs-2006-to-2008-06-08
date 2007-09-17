dcba5573942e8a56899ec8feabaf1c55f0121317
jbeltran <jbeltran@kuali.org>
2007-09-17 12:40:11 -0400

    RESOLVED - issue KULRNE-5633: either remove refs to GL.DEBUG param or make it a service property https://test.kuali.org/jira/browse/KULRNE-5633
    Removed references to GL.DEBUG since Sterling said it wasn't needed functionally.
    
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.13.2@9757 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/module/ld/document/web/struts/LaborCorrectionAction.java
+++ work/src/org/kuali/kfs/module/ld/document/web/struts/LaborCorrectionAction.java
@@ -94,8 +94,6 @@ public class LaborCorrectionAction extends CorrectionAction{
             CorrectionAction.kualiConfigurationService = SpringContext.getBean(KualiConfigurationService.class);
         }
 
-        request.setAttribute("debug", Boolean.valueOf(kualiConfigurationService.getIndicatorParameter(KFSConstants.GL_NAMESPACE, KFSConstants.Components.NOT_APPLICABLE, "GL.DEBUG")));
-
         CorrectionForm rForm = (CorrectionForm) form;
         LOG.debug("execute() methodToCall: " + rForm.getMethodToCall());
 
