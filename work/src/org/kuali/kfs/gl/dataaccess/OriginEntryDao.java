debe04949988fc374b8989264af0b5686dd23ce4
jkeller <jkeller@kuali.org>
2007-09-20 12:47:46 -0400

    Merged in latest changes from HEAD in prep for merge back into HEAD.
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.25.20@9882 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/gl/dataaccess/OriginEntryDao.java
+++ work/src/org/kuali/kfs/gl/dataaccess/OriginEntryDao.java
@@ -21,6 +21,7 @@ import java.util.Map;
 
 import org.kuali.core.util.KualiDecimal;
 import org.kuali.module.gl.bo.OriginEntry;
+import org.kuali.module.gl.bo.OriginEntryable;
 import org.kuali.module.gl.bo.OriginEntryGroup;
 
 /**
@@ -54,7 +55,7 @@ public interface OriginEntryDao {
      * 
      * @param oe Entry to delete
      */
-    public void deleteEntry(OriginEntry oe);
+    public void deleteEntry(OriginEntryable oe);
 
     /**
      * Return an iterator to all documents in a group
@@ -70,7 +71,7 @@ public interface OriginEntryDao {
      * @param oeg Group
      * @return Iterator of entries in the specified group
      */
-    public Iterator<OriginEntry> getEntriesByGroup(OriginEntryGroup oeg, int sort);
+    public <T> Iterator<T> getEntriesByGroup(OriginEntryGroup oeg, int sort);
 
     /**
      * Get bad balance entries
@@ -115,7 +116,7 @@ public interface OriginEntryDao {
      * 
      * @param entry entry to save
      */
-    public void saveOriginEntry(OriginEntry entry);
+    public void saveOriginEntry(OriginEntryable entry);
 
 
     public OriginEntry getExactMatchingEntry(Integer entryId);
@@ -143,4 +144,9 @@ public interface OriginEntryDao {
      * @return a set of summarized information of poster input entries within the specified groups
      */       
     public Iterator getPosterOutputSummaryByGroupId(Collection groups);
+    
+    /**
+     * force current persistence broker to clear its cache
+     */
+    public void clearCache();
 }
