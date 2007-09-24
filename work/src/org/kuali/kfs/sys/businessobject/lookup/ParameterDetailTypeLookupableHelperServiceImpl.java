26008c5381906896143cd053b0bbf1c26dc7d4f5
jkeller <jkeller@kuali.org>
2007-09-24 13:03:09 -0400

    Made filters on component lookup actually work.
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.1.2@9943 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/sys/businessobject/lookup/ParameterDetailTypeLookupableHelperServiceImpl.java
+++ work/src/org/kuali/kfs/sys/businessobject/lookup/ParameterDetailTypeLookupableHelperServiceImpl.java
@@ -16,10 +16,10 @@
 package org.kuali.kfs.lookup;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 import org.apache.commons.lang.StringUtils;
 import org.kuali.core.bo.BusinessObject;
@@ -29,11 +29,14 @@ import org.kuali.core.datadictionary.DocumentEntry;
 import org.kuali.core.datadictionary.TransactionalDocumentEntry;
 import org.kuali.core.lookup.CollectionIncomplete;
 import org.kuali.core.lookup.KualiLookupableHelperServiceImpl;
+import org.kuali.core.lookup.LookupUtils;
 import org.kuali.kfs.batch.Step;
 import org.kuali.kfs.context.SpringContext;
 
 public class ParameterDetailTypeLookupableHelperServiceImpl extends KualiLookupableHelperServiceImpl {
 
+    private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger( ParameterDetailTypeLookupableHelperServiceImpl.class );
+    
     private static ArrayList<ParameterDetailType> components = new ArrayList<ParameterDetailType>(); 
     
     @Override
@@ -69,26 +72,66 @@ public class ParameterDetailTypeLookupableHelperServiceImpl extends KualiLookupa
             }
         }
         
-        if ( baseLookup instanceof CollectionIncomplete ) {
-            long originalCount = ((CollectionIncomplete)baseLookup).getActualSizeIfTruncated();
-            baseLookup = new ArrayList<BusinessObject>( baseLookup );
+        String activeCheck = fieldValues.get("parameterDetailTypeCode");
+        if ( activeCheck == null ) {
+            activeCheck = "";
+        }
+        int maxResultsCount = LookupUtils.getApplicationSearchResultsLimit();
+        // only bother with the component lookup if returning active components
+        // and we have not already hit the configured lookup maximum
+        if ( baseLookup instanceof CollectionIncomplete && activeCheck != "N" && ((CollectionIncomplete)baseLookup).getActualSizeIfTruncated() < maxResultsCount ) {
+            long originalCount = Math.max(baseLookup.size(), ((CollectionIncomplete)baseLookup).getActualSizeIfTruncated() );
+            long totalCount = originalCount;
+            //baseLookup = new ArrayList<BusinessObject>( baseLookup );
+            Pattern detailTypeRegex = null;
+            Pattern namespaceRegex = null;
+            Pattern nameRegex = null;
+            // TODO module lookup?
+            // TODO active check
+            if ( StringUtils.isNotBlank( fieldValues.get("parameterDetailTypeCode") ) ) {
+                String patternStr = fieldValues.get("parameterDetailTypeCode").replace("*", ".*").toUpperCase();
+                try {
+                    detailTypeRegex = Pattern.compile(patternStr);
+                } catch ( PatternSyntaxException ex ) {
+                    LOG.error( "Unable to parse parameterDetailTypeCode pattern, ignoring.", ex );
+                }                
+            }
+            if ( StringUtils.isNotBlank( fieldValues.get("parameterNamespaceCode") ) ) {
+                String patternStr = fieldValues.get("parameterNamespaceCode").replace("*", ".*").toUpperCase();
+                try {
+                    namespaceRegex = Pattern.compile(patternStr);
+                } catch ( PatternSyntaxException ex ) {
+                    LOG.error( "Unable to parse parameterNamespaceCode pattern, ignoring.", ex );
+                }                
+            }
+            if ( StringUtils.isNotBlank( fieldValues.get("parameterDetailTypeName") ) ) {
+                String patternStr = fieldValues.get("parameterDetailTypeName").replace("*", ".*").toUpperCase();
+                try {
+                    nameRegex = Pattern.compile(patternStr);
+                } catch ( PatternSyntaxException ex ) {
+                    LOG.error( "Unable to parse parameterDetailTypeName pattern, ignoring.", ex );
+                }                
+            }
             for ( ParameterDetailType pdt : components ) {
                 boolean includeType = true;
-                if ( StringUtils.isNotBlank( fieldValues.get("parameterDetailTypeCode") ) ) {
-                    // if not match set to false
+                if ( detailTypeRegex != null ) {
+                    includeType = detailTypeRegex.matcher( pdt.getParameterDetailTypeCode().toUpperCase() ).matches();
                 }
-                if ( StringUtils.isNotBlank( fieldValues.get("parameterNamespaceCode") ) ) {
-                    // if not match set to false
+                if ( includeType && namespaceRegex != null ) {
+                    includeType = namespaceRegex.matcher( pdt.getParameterNamespaceCode().toUpperCase() ).matches();
                 }
-                if ( StringUtils.isNotBlank( fieldValues.get("parameterDetailTypeName") ) ) {
-                    // if not match set to false
+                if ( includeType && nameRegex != null ) {
+                    includeType = nameRegex.matcher( pdt.getParameterDetailTypeName().toUpperCase() ).matches();
                 }
                 if ( includeType ) {
-                    baseLookup.add( pdt );
+                    if ( totalCount < maxResultsCount ) {
+                        baseLookup.add( pdt );
+                    }
+                    totalCount++;
                 }
             }
-            baseLookup.addAll( components );
-            baseLookup = new CollectionIncomplete( baseLookup, originalCount + components.size() );
+            ((CollectionIncomplete)baseLookup).setActualSizeIfTruncated(totalCount);
+            //baseLookup = new CollectionIncomplete( baseLookup, originalCount + componentCount );
         }
         
         return baseLookup;
