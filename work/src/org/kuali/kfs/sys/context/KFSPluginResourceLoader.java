41262930b75501e826311ef699fb4aab82646b45
ewestfal <ewestfal@kuali.org>
2007-11-01 16:17:41 -0400

    Changed KFSPLuginResourceLoader to delegating to the other resourceloader.
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.1.2@11204 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/sys/context/KFSPluginResourceLoader.java
+++ work/src/org/kuali/kfs/sys/context/KFSPluginResourceLoader.java
@@ -15,13 +15,27 @@
  */
 package org.kuali.kfs.context;
 
+import java.util.List;
+
 import javax.xml.namespace.QName;
 
 import org.kuali.core.workflow.KFSResourceLoader;
+import org.kuali.rice.definition.ObjectDefinition;
 import org.kuali.rice.resourceloader.BaseResourceLoader;
 import org.kuali.rice.resourceloader.ResourceLoader;
 
+/**
+ * A ResourceLoader for the KFS plugin for KEW.  Essentially, this will load the KFS
+ * Spring Context and then delegate calls to the KFSResourceLoader which is loaded
+ * by Spring.
+ *
+ * @author Eric Westfall
+ */
 public class KFSPluginResourceLoader extends BaseResourceLoader {
+
+    private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(KFSPluginResourceLoader.class);
+
+    private ResourceLoader delegateResourceLoader;
 
     public KFSPluginResourceLoader() {
         super(new QName("KFSPluginResourceLoader"));
@@ -29,23 +43,63 @@ public class KFSPluginResourceLoader extends BaseResourceLoader {
 
     @Override
     public void start() throws Exception {
+        if (isStarted()) {
+            LOG.warn("KFSPluginResourceLoader already started.");
+            return;
+        }
         SpringContext.initializePluginApplicationContext();
         ResourceLoader kfsResourceLoader = SpringContext.getBean(KFSResourceLoader.class);
         if (kfsResourceLoader == null) {
             throw new RuntimeException("Could not locate the KFSResourceLoader");
         }
-        addResourceLoader(kfsResourceLoader);
+        delegateResourceLoader = kfsResourceLoader;
         super.start();
     }
 
     @Override
     public void stop() throws Exception {
         SpringContext.close();
+        delegateResourceLoader = null;
         super.stop();
     }
 
+    public void addResourceLoader(ResourceLoader arg0) {
+        delegateResourceLoader.addResourceLoader(arg0);
+    }
 
+    public void addResourceLoaderFirst(ResourceLoader arg0) {
+        delegateResourceLoader.addResourceLoaderFirst(arg0);
+    }
 
+    public String getContents(String arg0, boolean arg1) {
+        return delegateResourceLoader.getContents(arg0, arg1);
+    }
 
+    public Object getObject(ObjectDefinition arg0) {
+        return delegateResourceLoader.getObject(arg0);
+    }
+
+    public ResourceLoader getResourceLoader(QName arg0) {
+        return delegateResourceLoader.getResourceLoader(arg0);
+    }
+
+    public List<QName> getResourceLoaderNames() {
+        return delegateResourceLoader.getResourceLoaderNames();
+    }
+
+    public List<ResourceLoader> getResourceLoaders() {
+        return delegateResourceLoader.getResourceLoaders();
+    }
+
+    public Object getService(QName arg0) {
+        if (delegateResourceLoader == null) {
+            return null;
+        }
+        return delegateResourceLoader.getService(arg0);
+    }
+
+    public void removeResourceLoader(QName arg0) {
+        delegateResourceLoader.removeResourceLoader(arg0);
+    }
 
 }
