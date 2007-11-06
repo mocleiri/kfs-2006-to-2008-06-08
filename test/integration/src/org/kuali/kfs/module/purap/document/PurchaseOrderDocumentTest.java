f0edb1ef03745e10e7df2dea397a83ebc7f64167
yingfeng <yingfeng@kuali.org>
2007-11-06 10:19:33 -0500

    KULPURAP-2011
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.1.2@11309 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- test/integration/src/org/kuali/kfs/module/purap/document/PurchaseOrderDocumentTest.java
+++ test/integration/src/org/kuali/kfs/module/purap/document/PurchaseOrderDocumentTest.java
@@ -24,6 +24,9 @@ import static org.kuali.test.fixtures.UserNameFixture.RORENFRO;
 import java.util.ArrayList;
 import java.util.List;
 
+import junit.framework.Assert;
+
+import org.apache.commons.lang.StringUtils;
 import org.kuali.core.document.Document;
 import org.kuali.core.service.DataDictionaryService;
 import org.kuali.core.service.DocumentService;
@@ -34,11 +37,19 @@ import org.kuali.kfs.document.AccountingDocument;
 import org.kuali.module.financial.document.AccountingDocumentTestUtils;
 import org.kuali.module.purap.bo.PurchasingItem;
 import org.kuali.module.purap.bo.PurchaseOrderItem;
+import org.kuali.module.purap.document.WorkflowXmlPurchaseOrderDocument.DummyContractManager;
+import org.kuali.module.purap.document.WorkflowXmlPurchaseOrderDocument.DummyStatus;
+import org.kuali.module.purap.document.WorkflowXmlPurchaseOrderDocument.DummyVendorContract;
 import org.kuali.module.purap.fixtures.PurchaseOrderDocumentFixture;
 import org.kuali.module.purap.fixtures.PurchaseOrderItemAccountsFixture;
 import org.kuali.test.ConfigureContext;
 import org.kuali.test.DocumentTestUtils;
 import org.kuali.test.fixtures.UserNameFixture;
+import org.kuali.test.monitor.ChangeMonitor;
+import org.kuali.test.monitor.DocumentWorkflowStatusMonitor;
+import org.kuali.workflow.WorkflowTestUtils;
+
+import edu.iu.uis.eden.EdenConstants;
 
 /**
  * Unit Test Class for Internal Billing Document.
@@ -78,18 +89,73 @@ public class PurchaseOrderDocumentTest extends KualiTestBase {
     public final void testConvertIntoErrorCorrection() throws Exception {
         AccountingDocumentTestUtils.testConvertIntoErrorCorrection(buildSimpleDocument(), getExpectedPrePeCount(), SpringContext.getBean(DocumentService.class), SpringContext.getBean(TransactionalDocumentDictionaryService.class));
     }
-/*
+
     @ConfigureContext(session = KHUNTLEY, shouldCommitTransactions=true)
     public final void testRouteDocument() throws Exception {
-        AccountingDocumentTestUtils.testRouteDocument(buildSimpleDocument(), SpringContext.getBean(DocumentService.class));
+        PurchaseOrderDocument poDocument = buildSimpleDocument();
+        DocumentService documentService = SpringContext.getBean(DocumentService.class);
+        poDocument.prepareForSave();       
+        assertFalse("R".equals(poDocument.getDocumentHeader().getWorkflowDocument().getRouteHeader().getDocRouteStatus()));
+        AccountingDocumentTestUtils.routeDocument(poDocument, "saving copy source document", null, documentService);
+        WorkflowTestUtils.waitForStatusChange(poDocument.getDocumentHeader().getWorkflowDocument(), "F");        
+        //WorkflowTestUtils.waitForNodeChange(poDocument.getDocumentHeader().getWorkflowDocument(), "Open");
+        //assertTrue("Document should now be final.", poDocument.getDocumentHeader().getWorkflowDocument().stateIsFinal());
     }
 
     @ConfigureContext(session = KHUNTLEY, shouldCommitTransactions=true)
-    public final void testSaveDocument() throws Exception {     
-        AccountingDocumentTestUtils.testSaveDocument(buildSimpleDocument(), SpringContext.getBean(DocumentService.class));
+    public final void testSaveDocument() throws Exception {    
+        PurchaseOrderDocument poDocument = buildSimpleDocument();
+        DocumentService documentService = SpringContext.getBean(DocumentService.class);
+        poDocument.prepareForSave();       
+        AccountingDocumentTestUtils.saveDocument(poDocument, documentService);
+        PurchaseOrderDocument result = (PurchaseOrderDocument) documentService.getByDocumentHeaderId(poDocument.getDocumentNumber());
+        assertMatch(poDocument, result);
     }
-*/
+
     // test util methods
+
+    public static void assertMatch(PurchaseOrderDocument doc1, PurchaseOrderDocument doc2) {
+        // match header
+        Assert.assertEquals(doc1.getDocumentNumber(), doc2.getDocumentNumber());
+        Assert.assertEquals(doc1.getDocumentHeader().getWorkflowDocument().getDocumentType(), doc2.getDocumentHeader().getWorkflowDocument().getDocumentType());
+
+        // match posting year
+        if (StringUtils.isNotBlank(doc1.getPostingPeriodCode()) && StringUtils.isNotBlank(doc2.getPostingPeriodCode())) {
+            Assert.assertEquals(doc1.getPostingPeriodCode(), doc2.getPostingPeriodCode());
+        }
+        Assert.assertEquals(doc1.getPostingYear(), doc2.getPostingYear());
+        /*
+        // match important fields in PO
+        Assert.assertEquals(doc1, doc2);
+        
+        Assert.assertEquals(doc1.getVendorHeaderGeneratedIdentifier(), doc2.getVendorHeaderGeneratedIdentifier());
+        Assert.assertEquals(doc1.getVendorDetailAssignedIdentifier(), doc2.getVendorDetailAssignedIdentifier());
+        Assert.assertEquals(doc1.getVendorName(), doc2.getVendorName());
+        Assert.assertEquals(doc1.getVendorNumber(), doc2.getVendorNumber());
+        Assert.assertEquals(doc1.getAccountsForRouting(), doc2.getAccountsForRouting());
+        Assert.assertEquals(doc1.getStatusCode(), doc2.getStatusCode());
+
+        Assert.assertEquals(doc1.getChartOfAccountsCode(), doc2.getChartOfAccountsCode());
+        Assert.assertEquals(doc1.getOrganizationCode(), doc2.getOrganizationCode());
+        Assert.assertEquals(doc1.getDeliveryCampusCode(), doc2.getDeliveryCampusCode());
+        Assert.assertEquals(doc1.getDeliveryRequiredDate(), doc2.getDeliveryRequiredDate());
+        Assert.assertEquals(doc1.getRequestorPersonName(), doc2.getRequestorPersonName());
+        Assert.assertEquals(doc1.getContractManagerCode(), doc2.getContractManagerCode());
+        Assert.assertEquals(doc1.getVendorContractName(), doc2.getVendorContractName());
+        Assert.assertEquals(doc1.getPurchaseOrderAutomaticIndicator(), doc2.getPurchaseOrderAutomaticIndicator());
+        Assert.assertEquals(doc1.getPurchaseOrderTransmissionMethodCode(), doc2.getPurchaseOrderTransmissionMethodCode());
+
+        Assert.assertEquals(doc1.getRequisitionIdentifier(), doc2.getRequisitionIdentifier());
+        Assert.assertEquals(doc1.getPurchaseOrderPreviousIdentifier(), doc2.getPurchaseOrderPreviousIdentifier());
+        Assert.assertEquals(doc1.isPurchaseOrderCurrentIndicator(), doc2.isPurchaseOrderCurrentIndicator());
+        Assert.assertEquals(doc1.getPurchaseOrderCreateDate(), doc2.getPurchaseOrderCreateDate());
+        Assert.assertEquals(doc1.getPurchaseOrderLastTransmitDate(), doc2.getPurchaseOrderLastTransmitDate());
+
+        Assert.assertEquals(doc1.getContractManager(), doc2.getContractManager());
+        Assert.assertEquals(doc1.getVendorContract(), doc2.getVendorContract());
+        Assert.assertEquals(doc1.getStatus(), doc2.getStatus());        
+        */
+    }
     
     private List<PurchaseOrderItem> generateItems() throws Exception {
         List<PurchaseOrderItem> items = new ArrayList<PurchaseOrderItem>();
