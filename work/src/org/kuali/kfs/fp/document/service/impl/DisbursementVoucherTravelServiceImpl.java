4c04e86380fb61bc71178f909c9f5e6fe58d30fe
jkeller <jkeller@kuali.org>
2008-01-30 12:32:33 -0500

    KFSMI-289 : removed @Transactional annotation where not needed.
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.16.24.1.8@12369 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/fp/document/service/impl/DisbursementVoucherTravelServiceImpl.java
+++ work/src/org/kuali/kfs/fp/document/service/impl/DisbursementVoucherTravelServiceImpl.java
@@ -29,13 +29,11 @@ import org.kuali.core.util.KualiDecimal;
 import org.kuali.module.financial.bo.TravelMileageRate;
 import org.kuali.module.financial.dao.TravelMileageRateDao;
 import org.kuali.module.financial.service.DisbursementVoucherTravelService;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * This is the default implementation of the DisbursementVoucherTravelService interface.
  * Performs calculations of travel per diem and mileage amounts.
  */
-@Transactional
 public class DisbursementVoucherTravelServiceImpl implements DisbursementVoucherTravelService {
     private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(DisbursementVoucherTravelServiceImpl.class);
 
