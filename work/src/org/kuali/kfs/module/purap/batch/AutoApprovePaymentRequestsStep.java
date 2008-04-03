d5d6e58540f610c4750b6ddf939848682edb4c12
wliang <wliang@kuali.org>
2008-04-03 00:58:09 -0400

    KFSMI-622
    git-svn-id: https://svn.kuali.org/repos/kfs/branches/unlabeled-1.3.2.1.8@13397 05dbba50-812c-11dd-b2f5-a7aa3c7f87f7

--- work/src/org/kuali/kfs/module/purap/batch/AutoApprovePaymentRequestsStep.java
+++ work/src/org/kuali/kfs/module/purap/batch/AutoApprovePaymentRequestsStep.java
@@ -15,15 +15,28 @@
  */
 package org.kuali.module.purap.batch;
 
+import java.util.List;
+
+import org.kuali.core.util.ErrorMap;
+import org.kuali.core.util.GlobalVariables;
+import org.kuali.core.util.KualiDecimal;
 import org.kuali.kfs.batch.AbstractStep;
+import org.kuali.kfs.service.ParameterService;
+import org.kuali.module.purap.PurapParameterConstants;
+import org.kuali.module.purap.document.PaymentRequestDocument;
 import org.kuali.module.purap.service.PaymentRequestService;
 
 /**
  * Step used to auto approve payment requests that meet a certain criteria
+ * 
+ * DO NOT ANNOTATE THIS CLASS AS TRANSACTIONAL.  Otherwise, all documents will run within the same transaction, and if
+ * a RuntimeException is thrown, then it will cause the transaction for all documents to rollback, causing no requests to auto-approve
  */
 public class AutoApprovePaymentRequestsStep extends AbstractStep {
+    private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(AutoApprovePaymentRequestsStep.class);
 
     private PaymentRequestService paymentRequestService;
+    private ParameterService parameterService;
 
     public AutoApprovePaymentRequestsStep() {
         super();
@@ -35,7 +48,28 @@ public class AutoApprovePaymentRequestsStep extends AbstractStep {
      * @see org.kuali.kfs.batch.Step#execute(java.lang.String)
      */
     public boolean execute(String jobName) throws InterruptedException {
-        return paymentRequestService.autoApprovePaymentRequests();
+        boolean hasNoErrors = true;
+        // should objects from existing user session be copied over
+        List<String> autoApprovableDocumentNumbers = paymentRequestService.getPaymentRequestsDocumentNumberEligibleForAutoApproval();
+        if (autoApprovableDocumentNumbers != null) {
+            String samt = parameterService.getParameterValue(PaymentRequestDocument.class, PurapParameterConstants.PURAP_DEFAULT_NEGATIVE_PAYMENT_REQUEST_APPROVAL_LIMIT);
+            KualiDecimal defaultMinimumLimit = new KualiDecimal(samt);
+            for (String documentNumber : autoApprovableDocumentNumbers) {
+                try {
+                    LOG.info("Attempting to auto-approve payment request: " + documentNumber);
+                    GlobalVariables.setErrorMap(new ErrorMap());
+                    // the service call starts and ends its own transaction since this class is not transactional, so it's one transaction per document
+                    if (!paymentRequestService.autoApprovePaymentRequest(documentNumber, defaultMinimumLimit)) {
+                        hasNoErrors = false;
+                    }
+                }
+                catch (Exception e) {
+                    hasNoErrors = false;
+                    LOG.error("Payment Request Document " + documentNumber + " threw an exception during auto-approval.  Transaction rolled back.", e);
+                }
+            }
+        }
+        return hasNoErrors;
     }
 
     /**
@@ -50,6 +84,10 @@ public class AutoApprovePaymentRequestsStep extends AbstractStep {
 
     public void setPaymentRequestService(PaymentRequestService paymentRequestService) {
         this.paymentRequestService = paymentRequestService;
+    }
+
+    public void setParameterService(ParameterService parameterService) {
+        this.parameterService = parameterService;
     }
 
 }
